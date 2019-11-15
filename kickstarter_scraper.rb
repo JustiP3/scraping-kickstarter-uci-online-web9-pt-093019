021539b582642aab515f165c710a9d8f876c85a2
Justin Phillips <justinphil02@yahoo.com>
2019-11-15 01:21:15 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,8 +8,10 @@ def create_project_hash
   # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-project = kickstarter.css("#projects_list .location-name")
-location = project[0].text
+# description:
+descriptions = kickstarter.css("p.bbcard_blurb").text
+binding.pry
+locations = kickstarter.css("#projects_list .location-name")
+#location = locations[0].text
 binding.pry
 end
