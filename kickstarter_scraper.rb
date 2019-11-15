f220f80cb8a47f69969cf14ba839894ca0ba33a4
Justin Phillips <justinphil02@yahoo.com>
2019-11-15 01:19:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -10,5 +10,6 @@ def create_project_hash
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 project = kickstarter.css("#projects_list .location-name")
+location = project[0].text
 binding.pry
 end
